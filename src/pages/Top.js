2f0853e2650d76fc6e0120c33a50bf70aabd4e44
okutaku <pntgn.taku@gmail.com>
2021-01-26 22:25:48 +0900

    ビデオの表示画面作成

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -2,6 +2,8 @@ import React, { useEffect, useContext } from 'react'
 import Layout from '../coponents/Layout/Layout'
 import { fetchPopularData } from '../apis/index'
 import { Store } from '../store/index'
+import VideoGrid from '../coponents/VideoGrid/VideoGrid'
+import VideoGridItem from '../coponents/VideoGridItem/VideoGridItem'
 
 const Top = () => {
   //useContextを使用することでstore内の値を参照できる
@@ -16,7 +18,19 @@ const Top = () => {
   })
   return (
     <Layout>
-      Top page
+      <VideoGrid>
+        {
+          globalState.popular && globalState.popular.map((popular) => {
+            return (
+              <VideoGridItem
+                id={popular.id}
+                key={popular.id}
+                src={popular.snippet.thumbnails.standard.url}
+                title={popular.snippet.title}/>
+            )
+          })
+        }
+      </VideoGrid>
     </Layout>
   )
 }
